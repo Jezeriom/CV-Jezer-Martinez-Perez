3a7ca021039eb7e3e0b6c69030f98a276cbfaf45
Jezeriom <jezer976@gmail.com>
2026-06-02 13:18:41 -0500

    Configurar deploy de frontend en GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/CV-Jezer-Martinez-Perez/',
   plugins: [react()],
   server: {
     port: 5173,
